c76e63888389711ac2ce7b450ded1ee1bcb59b77
Victor <victor_anoshin@mail.ru>
2024-04-23 19:21:05 +0300

    add lora+bert model creating

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,10 @@ import json
 import pandas as pd
 from tqdm import tqdm
 import re
+from peft import LoraConfig, get_peft_model 
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+
 
 class BertDatasetTrainGrnti1(torch.utils.data.Dataset):
     def __init__(self, df, max_len, tokenizer):
@@ -74,22 +78,67 @@ def truncating_geting_text_df(df, df_test, number_of_indexes_for_truncation):
 
     return df_trunc, df_test_trunc, n_classes
 
+def creating_bert_peft_lora_model_lerning(pre_trained_model_name, n_classes, r, 
+                                          lora_alpha,
+                                          lora_dropout,
+                                          bias,
+                                          task_type):
+
+    tokenizer = AutoTokenizer.from_pretrained(pre_trained_model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(pre_trained_model_name, 
+                                                               num_labels=n_classes)
+
+    for param in model.parameters():
+        param.requires_grad = False
+        if param.ndim == 1:
+            param.data = param.data.to(torch.float32)
+
+    model.classifier = nn.Linear(model.config.hidden_size, n_classes)
+    config = LoraConfig(
+        r=r,
+        lora_alpha=lora_alpha,
+        lora_dropout=lora_dropout,
+        bias=bias,
+        task_type=task_type
+    )
+
+    model_peft = get_peft_model(model, config)
+    return model_peft, tokenizer
+
+
 def teach_model(model, dataloader, device, save_path):
     model.to(device)
     model.train()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    for i, batch in enumerate(tqdm(dataloader)):
+    total_step = len(dataloader)
+    correct = 0
+    total = 0
+    running_loss = 0.0
+    loss_per_iter = []
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+
+    for batch_id, batch in enumerate(tqdm(dataloader)):
+        optimizer.zero_grad()
         inputs = batch['input_ids'].to(device)
         mask = batch['mask'].to(device)
         y_train = batch['targets'].to(device)
-
         y_pred = model(input_ids = inputs, attention_mask = mask)
-
         loss = nn.functional.cross_entropy(y_pred.logits, y_train)
-        if not (i % 100):
-            print(f'coress entropy iteration {i}:', loss)
         loss.backward()
         optimizer.step()
-        optimizer.zero_grad()
+        
+
+        # print statistics
+        running_loss += loss.item()
+        _,pred = torch.max(y_pred.logits, dim=1)
+        correct += torch.sum(pred==y_train).item()
+        total += y_train.size(0)
+        loss_per_iter.append(loss.item())
+
+        if (batch_id % 100) == 0:
+            print(f'Step [{batch_id}/{total_step}], Loss: {loss_per_iter[-1]}')
+
+    train_acc = (100 * correct / total)
+    train_loss = (running_loss / total_step)    
     torch.save(model.state_dict(), save_path)
+    return train_acc, train_loss, loss_per_iter
