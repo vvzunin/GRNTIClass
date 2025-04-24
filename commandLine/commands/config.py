a5574b1185154bad550cea19f2c274f8aa582667
Elisei564 <batmansgrob@yandex.ru>
2025-04-24 20:36:14 +0300

    Transformation from argparse to click

---
+++ commandLine/commands/config.py
@@ -0,0 +1,49 @@
+import json
+from messages import printMessage
+import os
+
+defaultData = {
+  "modelType": "default",
+  "models": {
+    "default": {
+      "textLang": "ru",
+      "1": os.path.dirname(os.path.abspath(__file__)) + "\\models\\bert2\\bert_peft_level1",
+      "2": os.path.dirname(os.path.abspath(__file__)) + "\\models\\bert2\\bert_peft_level2_with_labels",
+      "3": "",
+      "description": "Модель на основе LORA для русского языка"
+    }
+  }
+}
+
+class Config():
+  def __init__(self):
+    self.__dict__ = defaultData
+    
+  def load(self, file):
+    try:
+      file = open(file, 'r', encoding="cp1251")
+      try:
+        data = json.load(file)
+        for i in ["modelType", "models"]:
+          if i not in data:
+            data[i] = defaultData[i]
+        data["models"].update(defaultData["models"])
+        self.__dict__ = data
+      finally:
+        file.close()
+    except IOError:
+      printMessage("badConfig")
+      quit()
+      
+  def save(self, file):
+    with open(file, 'w', encoding='cp1251') as f:
+      json.dump(self,
+                f,
+                default=lambda o: o.__dict__,
+                sort_keys=False,
+                ensure_ascii=False,
+                indent=2)
+      
+  def print(self):
+    for i in self.__dict__:
+      print(f'{i}: {self.__dict__[i]}')
--- commandLine/commands/main.py
+++ commandLine/commands/main.py
@@ -270,15 +270,49 @@ arguments = {
 
 import click
 import sys
+import os
+import json
+from messages import *
 from config import *
 
+
+
+
+
+def loadJSON(jsonPath):
+  try:
+    file = open(jsonPath, 'r', encoding="cp1251")
+    try:
+      data = json.load(file)
+      return data
+    finally:
+      file.close()
+  except IOError:
+    printMessage(messages["badConfig"])
+    quit()
+  return None
+prog = loadJSON("prog.json")
+program_version = prog["version"]
+# start = datetime.datetime.now()
+
+# messages = messages
+# printMessage(messages["start"])
+
 @click.group(invoke_without_command=True, help="test")
+@click.option("-s", "--silence", "silence", is_flag=True, help="Включение режима без логов")
+@click.option('-v', '--version', is_flag=True, help="Показать версию программы")
 @click.pass_context
-def main(ctx):
+def main(ctx, silence, version):
+  if silence:
+    sys.stdout = None
+    sys.stderr = None
   if ctx.invoked_subcommand is None:
     click.echo('I was invoked without subcommand')
   else:
     click.echo(f"I am about to invoke {ctx.invoked_subcommand}")
+  if version:
+     click.echo(f"Версия программы: {program_version}")
+
 
 
 
@@ -303,38 +337,144 @@ def list(online):
   if online:
     print("Данный функционал в процессе реализации")
   else:
+    path = "config.json"
+    if not os.path.exists(path):
+        print(f"Файл {path} не найден")
+        return
+
+    try:
+        with open(path, 'r', encoding='cp1251') as f:
+            data = json.load(f)
+    except Exception as e:
+        print(f"Ошибка при чтении JSON: {e}")
+        return
+
+    models_block = data.get("models", {})
+    for name, info in models_block.items():
+      print("Имя модели:", name)
+      print(f'\tЯзык: "{info.get("textLang", "не указано")}"')
+      print(f'\tОписание: {info.get("description", "нет описания")}')
+      print("\tПути к моделям для классификации:")
+
+      for key, value in info.items():
+        if value == '':
+          value = "Не указано"
+        if key.isdigit():
+          print(f"\tУровень {key} - {value}")
+@models.command(help="Загрузка конфиг файла")
+@click.option('-p', '--path', 'path',
+              type=click.Path(exists=True),
+              default="config.json",
+              help="Путь к конфигурационному файлу")
+def config(path):
+  config = loadJSON(path)
+  models.update(config['models'])
+  config['models'] = models
+
+
+  
+@main.group(invoke_without_command=True)
+
+
+@click.pass_context
+def run(ctx):
+    """Запуск классификации"""
+    if ctx.invoked_subcommand is None:
+      click.echo("Команда 'run' без подкоманды. Используйте --help")
+  
+
+
+
+
+@run.command(help="Сделать предсказание")
+@click.option('-i', '--input', 'input_file', default='text.txt', type=click.Path(exists=True), help='Путь к входному файлу')
+@click.option('-o', '--output', 'output_file', default='result.csv', type=click.Path(), help='Путь к результирующий файл')
+@click.option('-ei', '--inencode', 'input_encode', default='cp1251', type=str, help='Входна кодировка')
+@click.option('-eo', '--outencode', 'output_encode', default='cp1251', type=str, help='Выходная кодировка')
+@click.option('-id', '--identifier', 'identifier', default='RGNTI3', 
+              type=click.Choice(['RGNTI1', 'RGNTI2', 'RGNTI3']), help='Идентификатор рубрикатора')
+@click.option('-p', '--pack', 'packet', default=1, type=int, help='Количество одновременно классифицируемых текстов')
+@click.option('-f', '--format', 'input_format', default='plain', type=click.Choice(['plain', 'multidoc']), help='Формат файла')
+@click.option('-l', '--lang', 'language', default='ru', type=click.Choice(['ru', 'en']), help='Язык')
+@click.option('-t', '--threshold', 'threshold', type=click.FloatRange(0,1),
+               default=0.5, help='Минимальная вероятность рубрики (порог)')
+@click.option('-n', '--norm', 'normalization', default='not', type=click.Choice(['not', 'some', 'all']), help='Нормализация')
+@click.option('-d', '--dialog', 'dialog', help='Диалоговое окно')
+def predict(input_file, output_file, input_encode, output_encode, identifier, 
+            packet, input_format, language, threshold, normalization, dialog, ):
+  if dialog:
     pass
+  else:
+    from prediction import prepair_model, prepair_data_level1, prepair_data_level2, \
+        prepair_dataset, make_predictions, save_rubrics, toRubrics
+    from tqdm import tqdm
+    import torch
+    torch.cuda.empty_cache()
+    printMessage(messages["libs"])
+    params = {
+        "input_file": input_file,
+        "output_file": output_file,
+        "input_encode": input_encode,
+        "output_encode": output_encode,
+        "level": identifier,
+        "packet": packet,
+        "format": input_format,
+        "language": language,
+        "threshold": threshold,
+        "normalisation": normalization
+    }
+    model1 = None if config['models'][config['modelType']]["1"] == "" \
+                  else prepair_model(n_classes=36, 
+                                    lora_model_path=config['models'][config['modelType']]["1"])
+    model2 = None
+    model3 = None
+    if ((identifier == "RGNTI2") or (identifier == "RGNTI3")):
+        model2 = None if config['models'][config['modelType']]["2"] == ""\
+                      else prepair_model(n_classes=246,
+                                        lora_model_path=config['models'][config['modelType']]["2"])
+    if (identifier == "RGNTI3"):
+        model3 = None if config['models'][config['modelType']]["3"] == ""\
+                      else prepair_model(n_classes=0,
+                                        lora_model_path=config['models'][config['modelType']]["3"])
+
+    if ((model1 is None) or
+        ((model2 is None) and ((identifier == "RGNTI2") or (identifier == "RGNTI3"))) or
+        ((model3 is None) and (identifier == "RGNTI3"))):
+        printMessage(messages["modelError"])
+        quit()
+
+    printMessage(messages["startPredict"])
+    df_test = prepair_data_level1(input_file, format=input_format, encoding=input_encode)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    printMessage(messages["device"], (device, ))
+
+    if (normalization != "not"):
+        printMessage(messages["badFlag"], ('-n', normalization))
+        quit()
+
+    for i in tqdm(range(0, df_test.shape[0], packet)):
+        dataset_loader = prepair_dataset(df_test.iloc[i:i+packet])
+        predictions_level1 = make_predictions(model1, dataset_loader, device=device)
+        if (identifier == "RGNTI1"):
+            predictions_level1 = toRubrics(os.path.dirname(os.path.abspath(__file__)), predictions_level1, 1, threshold)
+            save_rubrics(df_test.iloc[i:i+packet], predictions_level1, prog, i == 0, output_encode)
+        else:
+            df_test2 = prepair_data_level2(os.path.dirname(os.path.abspath(__file__)), df_test.iloc[i:i+packet], predictions_level1, threshold)
+            dataset_loader2 = prepair_dataset(df_test2)
+            predictions_level2 = make_predictions(model2, dataset_loader2, device=device)
+            if (identifier == "RGNTI2"):
+                predictions_level2 = toRubrics(os.path.dirname(os.path.abspath(__file__)), predictions_level2, 2, threshold)
+                save_rubrics(df_test2, predictions_level2, params, prog, i == 0, output_encode)
+            else:
+                printMessage(messages["notComplete"])
+                predictions_level2 = toRubrics(os.path.dirname(os.path.abspath(__file__)), predictions_level2, 2, threshold)
+                save_rubrics(df_test2, predictions_level2, params, prog, i == 0, output_encode)
+
+    del model1
+    del model2
+    del model3
   
-  
-  
-@main.group(invoke_without_command=False,
-            chain=True,
-            help="Запуск классификации")
-def run():
-  print()
-  
-@run.command(help="Загрузка конфиг файла")
-@click.option('-p', '--path', 'path',
-              type=str,
-              default="config.json",
-              help="Путь к конфигурационному файлу")
-def config():
-  print()
-  
-  
-@main.group(invoke_without_command=False,
-            chain=True,
-            help="Запуск классификации")
-def run():
-  print()
-  
-@run.command(help="Загрузка конфиг файла")
-@click.option('-p', '--path', 'path',
-              type=str,
-              default="config.json",
-              help="Путь к конфигурационному файлу")
-def config():
-  print()
+
   
 if __name__ == "__main__":
   main()
